ba9a88dc08f4c2a58934117d9192b4911e507891
non7247 <funi_funi@nifty.com>
2022-07-10 16:54:10 +0900

    公式ページの Getting Started - Quick Start まで動かした状態

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,32 @@
+#![cfg_attr(
+  all(not(debug_assertions), target_os = "windows"),
+  windows_subsystem = "windows"
+)]
+
+use tauri::{CustomMenuItem, Menu, MenuItem};
+
+fn main() {
+  let menu = Menu::new();
+
+  tauri::Builder::default()
+    .menu(menu)
+    .on_menu_event(|event| {
+      match event.menu_item_id() {
+        "quit" => {
+          std::process::exit(0);
+        }
+        "close" => {
+          event.window().close().unwrap();
+        }
+        _ => {}
+      }
+    })
+    .invoke_handler(tauri::generate_handler![greet])
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}!", name)
+}
