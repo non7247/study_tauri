24aea51a12a04a4b444d54efb0e89dc5f59bf73c
non7247 <funi_funi@nifty.com>
2022-07-16 11:37:14 +0900

    text と button を追加してcommand呼び出しの確認

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,24 +3,8 @@
   windows_subsystem = "windows"
 )]
 
-use tauri::{CustomMenuItem, Menu, MenuItem};
-
 fn main() {
-  let menu = Menu::new();
-
   tauri::Builder::default()
-    .menu(menu)
-    .on_menu_event(|event| {
-      match event.menu_item_id() {
-        "quit" => {
-          std::process::exit(0);
-        }
-        "close" => {
-          event.window().close().unwrap();
-        }
-        _ => {}
-      }
-    })
     .invoke_handler(tauri::generate_handler![greet])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
