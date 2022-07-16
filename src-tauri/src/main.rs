744ec0f9be73e9e16cd26d8b02d6fe86893d305b
non7247 <funi_funi@nifty.com>
2022-07-16 15:32:10 +0900

    Core側にCalcState構造体を追加

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,11 +3,34 @@
   windows_subsystem = "windows"
 )]
 
+use std::sync::Mutex;
+use tauri::Manager;
+
+#[derive(Debug)]
+struct CalcState {
+    num1: i64,
+    num2: i64,
+    operator: String,
+}
+
+struct CalcStateManager {
+    state: Mutex<CalcState>,
+}
+
 fn main() {
   tauri::Builder::default()
     .invoke_handler(tauri::generate_handler![
         greet,
     ])
+    .setup(|app| {
+        let calc_state_manager 
+            = CalcStateManager{ state: Mutex::new(CalcState{ num1: 0, 
+                                                             num2: 0, 
+                                                             operator: String::from("")})};
+        app.manage(calc_state_manager);
+
+        Ok(())
+    })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
