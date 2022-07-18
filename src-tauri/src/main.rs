01f3c6599078360acf5fcdca8cddeccc51d190b6
non7247 <funi_funi@nifty.com>
2022-07-18 09:55:13 +0900

    displayのクリアを追加

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,12 +4,12 @@
 )]
 
 use std::sync::Mutex;
-use tauri::Manager;
+use tauri::{Manager, State};
 
 #[derive(Debug)]
 struct CalcState {
-    num1: i64,
-    num2: i64,
+    x_number: i64,
+    y_number: i64,
     operator: String,
 }
 
@@ -17,16 +17,42 @@ struct CalcStateManager {
     state: Mutex<CalcState>,
 }
 
+impl CalcStateManager {
+    pub fn new() -> Self {
+        CalcStateManager {
+            state: Mutex::new(CalcState{ x_number: 0,
+                                         y_number: 0,
+                                         operator: String::from("") })
+        }
+    }
+
+    pub fn clear(&self) {
+        let mut calc_state = self.state.lock().unwrap();
+
+        calc_state.x_number = 0;
+        calc_state.y_number = 0;
+        calc_state.operator = String::from("");
+    }
+
+    pub fn display_text(&self) -> String {
+        let calc_state = self.state.lock().unwrap();
+
+        if &calc_state.operator == "" {
+            calc_state.x_number.to_string()
+        } else {
+            calc_state.y_number.to_string()
+        }
+    }
+}
+
 fn main() {
   tauri::Builder::default()
     .invoke_handler(tauri::generate_handler![
         greet,
+        calc_clear,
     ])
     .setup(|app| {
-        let calc_state_manager 
-            = CalcStateManager{ state: Mutex::new(CalcState{ num1: 0, 
-                                                             num2: 0, 
-                                                             operator: String::from("")})};
+        let calc_state_manager = CalcStateManager::new();
         app.manage(calc_state_manager);
 
         Ok(())
@@ -38,4 +64,10 @@ fn main() {
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}!", name)
+}
+
+#[tauri::command]
+fn calc_clear(calc_state_manager: State<'_, CalcStateManager>) -> String {
+    calc_state_manager.clear();
+    calc_state_manager.display_text()
 }
